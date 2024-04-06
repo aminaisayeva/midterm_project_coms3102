162060335404997263a3362b179f84b84aa196a4
ai2464 <ai2464@columbia.edu>
2024-04-05 22:52:24 -0400

    part 2 and part 3 complete, added add and delete buttons

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,45 @@
-// src/App.jsx
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
-import notes from "../notes";
+import NewNote from "./NewNote";
 
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  function coolNote(newNote) {
+    setNotes((lastNote) => {
+      return [...lastNote, newNote];
+    });
+  }
+
+  function badNote(id) {
+    setNotes((lastNote) => {
+      return lastNote.filter((_, kluch) => {
+        return kluch !== id;
+      });
+    });
+  }
+
   return (
     <div>
       <Header />
-      {notes.map((noteItem) => (
-        <Note
-          key={noteItem.key}
-          title={noteItem.title}
-          content={noteItem.content}
-        />
-      ))}
+      <NewNote onAdd={coolNote} />
+      {notes.map((noteItem, kluch) => {
+        return (
+          <Note
+            id={kluch}
+            key={kluch}
+            content={noteItem.content}
+            title={noteItem.title}
+            onDelete={badNote}
+          />
+        );
+      })}
       <Footer />
     </div>
   );
 }
 
 export default App;
+
